60d2c9ea5015486873dce620b724983ee04d087e
VSinghwal <vedantsinghwal@gmail.com>
2024-06-15 11:52:50 +0530

    dd

--- Clint/src/Sidebar/ExcecutiveSidebar.js
+++ Clint/src/Sidebar/ExcecutiveSidebar.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import './Sidebar.css';
 
-const ExcecutiveSidebar = () => {
+const Excecutivesidebar = () => {
   return (
     <div>
-        
+    <div className="sidebarabovediv"></div>
+    <div className='excecutiveside'>
+        <img src='./grouplogo.webp' className='sidebarlogo mb-3' alt=''/>
+            <div className="d-flex sideanchor "><img src="./dashboardicon.webp" className="sidebaricon" alt="icon"/><li className=''>Dashboard</li></div>
+            <div className="d-flex sideanchor"><img src="./projectsicon.webp" className="sidebaricon" alt="icon"/><li>Project</li></div>
+            <div className="d-flex sideanchor"><img src="./dashboardicon.webp" className="sidebaricon" alt="icon"/><li>Dashboard</li></div>
+            <div className="d-flex sideanchor"><img src="./projectsicon.webp" className="sidebaricon" alt="icon"/><li>Project</li></div>
+            <div className="d-flex sideanchor"><img src="./dashboardicon.webp" className="sidebaricon" alt="icon"/><li>Dashboard</li></div>
+            <div className="d-flex sideanchor"><img src="./projectsicon.webp" className="sidebaricon" alt="icon"/><li>Project</li></div>
+            <div className="d-flex sideanchor"><img src="./dashboardicon.webp" className="sidebaricon" alt="icon"/><li>Dashboard</li></div>
+            <div className="d-flex sideanchor"><img src="./projectsicon.webp" className="sidebaricon" alt="icon"/><li>Project</li></div>
+    </div>
     </div>
   )
 }
 
-export default ExcecutiveSidebar
+export default Excecutivesidebar
